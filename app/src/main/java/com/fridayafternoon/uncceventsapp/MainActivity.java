ed76a2a6de994ea59cfb075a47d07f1a913c691d
Christopher Bare <christopherbare@outlook.com>
2018-09-06 20:26:05 -0400

    Authentication, sign up, and mainactivity intents are complete.

--- app/src/main/java/com/fridayafternoon/uncceventsapp/MainActivity.java
+++ app/src/main/java/com/fridayafternoon/uncceventsapp/MainActivity.java
@@ -1,15 +1,35 @@
 package com.fridayafternoon.uncceventsapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    DialogInterface.OnClickListener dialogClickListener;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+    FirebaseStorage mStorage;
+    StorageReference storageReference;
+    String usersName;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +56,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorage = FirebaseStorage.getInstance();
+        storageReference = mStorage.getReference();
+
+        dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        SharedPreferences prefs = getSharedPreferences("info", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("email", mAuth.getCurrentUser().getEmail());
+                        editor.commit();
+
+                        mAuth.signOut();
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No was clicked
+                        break;
+                }
+            }
+        };
+
+        //set users name
+        //usersName.setText(mAuth.getCurrentUser().getDisplayName());
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logoutButton){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder .setMessage("Are you sure?")
+                    .setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
---
+++ app/src/main/java/com/fridayafternoon/uncceventsapp/SignUpActivity.java
@@ -0,0 +1,132 @@
+package com.fridayafternoon.uncceventsapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.oob.SignUp;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "demo";
+    ImageView image;
+    EditText etFirstName;
+    EditText etLastName;
+    EditText etEmail;
+    EditText etPassword;
+    EditText etPasswordRepeat;
+    Button buttonSignUp;
+    Button buttonCancel;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+        etFirstName = findViewById(R.id.contactFirstName);
+        etLastName = findViewById(R.id.contactLastName);
+        etEmail = findViewById(R.id.contactEmail);
+        etPassword = findViewById(R.id.contactPassword);
+        etPasswordRepeat = findViewById(R.id.passwordSignUpRepeat);
+        buttonSignUp = findViewById(R.id.buttonAddContact);
+        buttonCancel = findViewById(R.id.buttonCancel);
+
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String repeat = etPasswordRepeat.getText().toString().trim();
+                final String firstName = etFirstName.getText().toString().trim();
+                final String lastName = etLastName.getText().toString().trim();
+                Toast.makeText(SignUpActivity.this, "Sign Up Button.", Toast.LENGTH_SHORT).show();
+                if(firstName.equals("")){
+                    Toast.makeText(SignUpActivity.this, "Enter First Name", Toast.LENGTH_SHORT).show();
+                } else if (lastName.equals("")){
+                    Toast.makeText(SignUpActivity.this, "Enter Last Name", Toast.LENGTH_SHORT).show();
+                } else if (email.equals("")){
+                    Toast.makeText(SignUpActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                } else if (password.equals("")){
+                    Toast.makeText(SignUpActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else if (repeat.equals("")){
+                    Toast.makeText(SignUpActivity.this, "Enter Confirm Password", Toast.LENGTH_SHORT).show();
+                } else if (!repeat.equals(password)){
+                    Toast.makeText(SignUpActivity.this, "Enter Passwords Don't Match", Toast.LENGTH_SHORT).show();
+                } else{
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("demo", "signUpWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                .setDisplayName(firstName +" "+ lastName)
+                                                .build();
+
+                                        user.updateProfile(profileUpdates)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Log.d("demo", "User profile updated");
+                                                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        }
+                                                    }
+                                                });
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("demo", "signUpWithEmail:failure", task.getException());
+                                        Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignUpActivity.this, "Cancel Button.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+}
