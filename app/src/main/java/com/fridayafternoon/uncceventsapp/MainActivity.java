5cfde2d25495bf8f62cae27e2d628dd41757b77a
christopher <christopherbare@outlook.com>
2018-09-19 04:30:30 -0400

    Added the AsyncTask class for the web scraping part of things.

--- app/src/main/java/com/fridayafternoon/uncceventsapp/MainActivity.java
+++ app/src/main/java/com/fridayafternoon/uncceventsapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -115,5 +116,36 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
+
+    /**
+     * This is for scraping the webpage. Will start adding the code to do that soon.
+     */
+    private static class ScrapeAsync extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
+
 
 }
