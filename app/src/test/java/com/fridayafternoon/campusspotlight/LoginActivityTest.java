c35fe03fa3fc74b81132d3a8431521552f69a205
Christopher Bare <christopherbare@outlook.com>
2018-10-29 13:43:55 -0400

    Created test files.

---
+++ app/src/test/java/com/fridayafternoon/campusspotlight/LoginActivityTest.java
@@ -0,0 +1,16 @@
+package com.fridayafternoon.campusspotlight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+}
