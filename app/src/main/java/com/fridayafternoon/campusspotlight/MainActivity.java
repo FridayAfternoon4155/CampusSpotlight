c88b5e20cf0c4a2431a1df8ec2aba41a2b5fba9a
Christopher Bare <christopherbare@outlook.com>
2018-10-29 19:12:11 -0400

    Fixed action bar stuff.

--- app/src/main/java/com/fridayafternoon/campusspotlight/MainActivity.java
+++ app/src/main/java/com/fridayafternoon/campusspotlight/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseStorage mStorage;
     StorageReference storageReference;
     String usersName;
+    Button logout;
 
 
 
